bfff4b932eb602fa3ff6cd78f28c7a31493e89e1
Euan <e.blackledge@stuart.com>
2025-01-15 11:47:54 +0100

    Adding unit testing for quick demonstration

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -8,16 +8,22 @@ with source as (
 
 ),
 
-renamed as (
+staged as (
 
-    select
+    select 
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
-
+        datediff('day', cast(order_date as DATE), cast({{ dbt.current_timestamp() }} as DATE)) as days_since_ordered,
+        status like '%pending%' as is_status_pending,
+        case 
+            when status like '%shipped%' then 'shipped'
+            when status like '%return%' then 'returned'
+            when status like '%pending%' then 'placed'
+            else status
+        end as status
     from source
 
 )
 
-select * from renamed
+select * from staged
